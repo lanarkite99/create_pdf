cbb9039f9d31b45d04559eb1f6eac7481817fa88
Meet <meetapple191@gmail.com>
2024-05-04 18:58:12 +0530

    Add pages and set footer

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ pdf=FPDF(orientation="P", format="A4", unit="mm")
 df=pd.read_csv("topics.csv")
 
 for index,row in df.iterrows():
-    pdf.add_page()
-    pdf.set_font(family="Times", style="B", size=18)
-    pdf.cell(w=0,h=12, txt=row["Topic"], align="C",ln=1 )
-    pdf.line(8,21,202,21)
+    for i in range(row["Pages"]):
+        pdf.add_page()
+        pdf.set_font(family="Times", style="B", size=18)
+        pdf.set_text_color(0, 0, 0)
+        pdf.cell(w=0,h=12, txt=row["Topic"], align="C",ln=1, border="B" )
+        #pdf.line(8,21,202,21)
 
+        pdf.ln(240)
+        pdf.set_font(family="Times", style="I", size=9)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0,h=12, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
